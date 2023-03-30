6a60339777458d7640fea13844c9a96b27524833
Alice <matthew63731417@gmail.com>
2023-03-30 10:38:22 +0800

    calculator
    Changes to be committed:
    	modified:   from_test.html
    	modified:   loginCheck.php

--- loginCheck.php
+++ loginCheck.php
@@ -7,13 +7,37 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $num1 = $_POST["num1"];
-        $num2 = $_POST["num2"];
-        $num3 = $_POST["num3"];
+<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$num1 = $_POST["num1"];
+		$num2 = $_POST["num2"];
+		$operator = $_POST["operator"];
 
-        $sum = $num1 + $num2 + $num3;
-    ?>
+		switch ($operator) {
+			case "add":
+				$result = $num1 + $num2;
+				break;
+			case "subtract":
+				$result = $num1 - $num2;
+				break;
+			case "multiply":
+				$result = $num1 * $num2;
+				break;
+			case "divide":
+				if ($num2 == 0) {
+					$result = "Error: division by zero";
+				} else {
+					$result = $num1 / $num2;
+				}
+				break;
+			default:
+				$result = "Error: invalid operator";
+				break;
+		}
+
+		echo "<br><br><strong>Result:</strong> " . $result;
+	}
+	?>
 
     <h2><?php echo $sum; ?></h2>
 </body>
