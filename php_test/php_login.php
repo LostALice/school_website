5864e33db14df36df19b204f7841d54946fae95a
Alice <matthew63731417@gmail.com>
2023-03-31 10:33:04 +0800

    Changes to be committed: 	new file:   php_test/index.html 	new file:   php_test/php_login.php

---
+++ php_test/php_login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $email = $_POST["exampleInputEmail1"];
+            $pw = $_POST["exampleInputPassword1"];
+
+
+
+            echo "<br><br><strong>email:</strong> " . $email;
+            echo "<br><br><strong>pw:</strong> " . $pw;
+            echo "<br><br><strong>pw:</strong> " . $result;
+        }
+	?>
+</body>
+</html>
